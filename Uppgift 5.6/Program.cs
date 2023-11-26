a1742e63ee03f421c2796d6ae473efaea1a31b69
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2023-11-26 19:19:33 +0100

    Add project files.

---
+++ Uppgift 5.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_5._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tal du vill multiplicera. (sepparera dem med *)");
+            string talenLista = Console.ReadLine();
+            string[] talenText = talenLista.Split('*');
+            int faktor = 1;
+            foreach (string tal in talenText)
+            {
+                int faktor2 = int.Parse(tal);
+                faktor = faktor2 * faktor;
+            }
+            Console.WriteLine($"Produkten är {faktor}.");
+            Console.ReadKey();
+        }
+    }
+}
